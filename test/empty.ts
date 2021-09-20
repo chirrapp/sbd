bdcff535cb9c977ae7c2de64f03801df4be9ceb9
Sasha Koss <koss@nocorp.me>
2021-09-20 15:35:35 +0800

    Format source code using Prettier

--- test/empty.ts
+++ test/empty.ts
@@ -1,60 +1,53 @@
-/*jshint node:true, laxcomma:true */
-/*global describe:true, it:true */
-"use strict";
+import assert from "assert";
+import * as tokenizer from "../lib/tokenizer";
 
-import assert from 'assert';
-import * as tokenizer from '../lib/tokenizer'
+describe("Empty", function () {
+  describe("string", function () {
+    var entry = "";
+    var sentences = tokenizer.sentences(entry);
 
-describe('Empty', function () {
+    it("should not get a sentence", function () {
+      assert.equal(sentences.length, 0);
+    });
 
-  describe('string', function () {
-      var entry = "";
-      var sentences = tokenizer.sentences(entry);
+    var entry = "            \n\n                 ";
+    var sentences = tokenizer.sentences(entry);
 
-      it('should not get a sentence', function () {
-          assert.equal(sentences.length, 0);
-      });
-
-      var entry = "            \n\n                 ";
-      var sentences = tokenizer.sentences(entry);
-
-      it('should not get a sentence from whitespace', function () {
-          assert.equal(sentences.length, 0);
-      });
-
+    it("should not get a sentence from whitespace", function () {
+      assert.equal(sentences.length, 0);
+    });
   });
 
-  describe('undefined', function () {
-      var sentences = tokenizer.sentences();
+  describe("undefined", function () {
+    var sentences = tokenizer.sentences();
 
-      it('should not get a sentence', function () {
-          assert.equal(sentences.length, 0);
-      });
+    it("should not get a sentence", function () {
+      assert.equal(sentences.length, 0);
+    });
   });
 
-  describe('non string', function () {
-      var entry = [];
-      var sentences = tokenizer.sentences(entry);
+  describe("non string", function () {
+    var entry = [];
+    var sentences = tokenizer.sentences(entry);
 
-      it('should not get a sentence from array', function () {
-          assert.equal(sentences.length, 0);
-      });
+    it("should not get a sentence from array", function () {
+      assert.equal(sentences.length, 0);
+    });
 
-      var entry = {};
-      var sentences = tokenizer.sentences(entry);
+    var entry = {};
+    var sentences = tokenizer.sentences(entry);
 
-      it('should not get a sentence from object', function () {
-          assert.equal(sentences.length, 0);
-      });
+    it("should not get a sentence from object", function () {
+      assert.equal(sentences.length, 0);
+    });
   });
 
-  describe('symbols only', function () {
-      var entry = "^&%(*&";
-      var sentences = tokenizer.sentences(entry);
+  describe("symbols only", function () {
+    var entry = "^&%(*&";
+    var sentences = tokenizer.sentences(entry);
 
-      it('should not single entry', function () {
-          assert.equal(sentences.length, 1);
-      });
+    it("should not single entry", function () {
+      assert.equal(sentences.length, 1);
+    });
   });
-
 });
--- test/newline.ts
+++ test/newline.ts
@@ -1,64 +1,64 @@
-/*jshint node:true, laxcomma:true */
-/*global describe:true, it:true */
-"use strict";
+import assert from "assert";
+import * as tokenizer from "../lib/tokenizer";
 
-import assert from 'assert';
-import * as tokenizer from '../lib/tokenizer'
+describe("Save newlines", function () {
+  describe("Basic", function () {
+    var entry = "First sentence... Another list: \n - green \n - blue \n - red";
+    var sentences = tokenizer.sentences(entry);
 
-describe('Save newlines', function () {
-
-    describe('Basic', function () {
-        var entry = "First sentence... Another list: \n - green \n - blue \n - red";
-        var sentences = tokenizer.sentences(entry);
-
-        it('second sentence should have newlines', function () {
-            assert.equal(sentences[1], "Another list: \n - green \n - blue \n - red");
-        });
+    it("second sentence should have newlines", function () {
+      assert.equal(sentences[1], "Another list: \n - green \n - blue \n - red");
     });
+  });
 
-    describe('Sentence without lists', function () {
-        var entry = "First sentence... Another sentence.\nThis is a new paragraph.";
-        var sentences = tokenizer.sentences(entry);
+  describe("Sentence without lists", function () {
+    var entry = "First sentence... Another sentence.\nThis is a new paragraph.";
+    var sentences = tokenizer.sentences(entry);
 
-        it('second sentence should have newlines', function () {
-            assert.equal(sentences.length, 3);
-        });
+    it("second sentence should have newlines", function () {
+      assert.equal(sentences.length, 3);
     });
+  });
 
-    describe('With option to use newlines as sentence boundaries', function () {
-        var entry = "First sentence... Another list: \n - green \n - blue \n - red";
-        var sentences = tokenizer.sentences(entry, { "newline_boundaries": true });
+  describe("With option to use newlines as sentence boundaries", function () {
+    var entry = "First sentence... Another list: \n - green \n - blue \n - red";
+    var sentences = tokenizer.sentences(entry, { newline_boundaries: true });
 
-        it('second sentence should have newlines', function () {
-            assert.equal(sentences.length, 5);
-        });
+    it("second sentence should have newlines", function () {
+      assert.equal(sentences.length, 5);
     });
+  });
 
-
-    describe('Multiline strings', function () {
-        var entry = "How now brown cow.\
+  describe("Multiline strings", function () {
+    var entry =
+      "How now brown cow.\
         \
         Peter Piper Picked a peck of pickled peppers. A peck of pickled peppers peter piper picked.";
 
-        var sentences = tokenizer.sentences(entry);
+    var sentences = tokenizer.sentences(entry);
 
-        it('Should have 3 sentences ending in periods', function () {
-            assert.equal(sentences[0], "How now brown cow.");
-            assert.equal(sentences[1], "Peter Piper Picked a peck of pickled peppers.");
-        });
+    it("Should have 3 sentences ending in periods", function () {
+      assert.equal(sentences[0], "How now brown cow.");
+      assert.equal(
+        sentences[1],
+        "Peter Piper Picked a peck of pickled peppers."
+      );
     });
+  });
 
-
-    describe('Template multiline strings', function () {
-        var entry = `How now brown cow.
+  describe("Template multiline strings", function () {
+    var entry = `How now brown cow.
 
         Peter Piper Picked a peck of pickled peppers. A peck of pickled peppers peter piper picked.`;
 
-        var sentences = tokenizer.sentences(entry, { "newline_boundaries":  true });
+    var sentences = tokenizer.sentences(entry, { newline_boundaries: true });
 
-        it('Should have 3 sentences ending in periods', function () {
-            assert.equal(sentences[0], "How now brown cow.");
-            assert.equal(sentences[1], "Peter Piper Picked a peck of pickled peppers.");
-        });
+    it("Should have 3 sentences ending in periods", function () {
+      assert.equal(sentences[0], "How now brown cow.");
+      assert.equal(
+        sentences[1],
+        "Peter Piper Picked a peck of pickled peppers."
+      );
     });
+  });
 });
